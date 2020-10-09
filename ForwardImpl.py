cece0e150c688587938fb29efe320f771af3cfb2
cisimon7 <simontaneous1@hotmail.com>
2020-10-09 23:35:45 +0100

    F_Impl

---
+++ ForwardImpl.py
@@ -0,0 +1,16 @@
+from numpy import set_printoptions
+from sympy import trigsimp, lambdify
+from utils import *
+
+# Joint parameters declaration
+q1, q2, a2, d1, d3 = symbols("q1 q2 a2, d1 d3", real=True)
+
+# Forward Kinematics Homogeneous Matrix
+H = Rz(q1)*Tz(d1)*Rx(q2)*Ty(a2)*Ty(d3)*Tz(d3)
+H = trigsimp(H)
+
+set_printoptions(suppress=True)
+
+
+def forwardKinematics():
+    return lambdify([(q1, q2, a2, d1, d3)], H, "numpy")
--- main.py
+++ main.py
@@ -1,16 +1,32 @@
-# This is a sample Python script.
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+from ForwardImpl import forwardKinematics
+from math import pi
+from numpy import round
 
 
-# Press the green button in the gutter to run the script.
+def forward_tests():
+    # test cases
+    # you can add more test cases here
+    # angles input should be in degrees
+    # q1, q2, a2, d1, d3
+    test_cases = [
+        [0, 0, 1, 0, 3],
+        [30, 50, 1, 0, 0]
+    ]
+
+    f_function = forwardKinematics()
+
+    for i, param in enumerate(test_cases):
+        h = f_function((
+            (param[0] * pi / 180),  # q1
+            (param[1] * pi / 180),  # q2
+            param[2],               # d3
+            param[3],               # d3
+            param[4]                # d3
+        ))
+
+        view = f"CASE {i+1}:\n{round(h, 4)}\n"
+        print(view)
+
+
 if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    forward_tests()
